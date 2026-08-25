74b55c8567c2d92ba9eeabd5f49384f27f190541
LinYuwei <10131887+LywOPUS@users.noreply.github.com>
2026-08-26 01:02:52 +0800

    删除未使用的玻璃实验组件与依赖。

--- ui/src/components/ui/webgpu-liquid-glass/index.ts
+++
@@ -1,2 +0,0 @@
-export { WebGpuLiquidGlass, type WebGpuLiquidGlassProps } from './LiquidGlass'
-export { DEFAULT_PARAMS, type LiquidGlassParams } from './renderer'
--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import 'glass-lens-react/styles.css'
 import './index.css'
 import { App } from './App'
 
